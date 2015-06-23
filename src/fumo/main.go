2583551b8b76dca783cc3c8e443781cb3bf9e739
Tsuyoshi Saito <tsuyoshi.s.saito@gmail.com>
2015-06-23 16:56:46 +0900

    エディタが表示される様になった。背景色は白で、日本語は打てない

--- src/fumo/main.go
+++ src/fumo/main.go
@@ -1,7 +1,27 @@
 package main
 
-import "fmt"
+import (	
+	"github.com/google/gxui"
+	"github.com/google/gxui/drivers/gl"
+	"github.com/google/gxui/themes/dark"
+)
 
-func main(){
-     fmt.Println("Hello World!")
+func appMain(driver gxui.Driver) {
+	theme := dark.CreateTheme(driver)
+	editor := theme.CreateCodeEditor()
+	
+	window := theme.CreateWindow(580, 300, "Code Editor")
+	
+	color := gxui.White
+	
+	brush := gxui.CreateBrush(color)
+	window.SetBackgroundBrush(brush)
+	
+	window.AddChild(editor)
+	
+	window.OnClose(driver.Terminate)
+}
+
+func main() {
+	gl.StartDriver(appMain)
 }
